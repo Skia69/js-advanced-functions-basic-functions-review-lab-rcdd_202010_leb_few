ad53a530ff021e5891b2ab680ab1ce35f740d57f
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-10 09:09:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const saturdayFun = (activity = "roller-skate") => {
   return `This Saturday, I want to ${activity}!`
 }
 
-const mondayWork = (activity) => {
+const mondayWork = (activity = "go to the office") => {
   return `This Monday, I will ${activity}`
 }
