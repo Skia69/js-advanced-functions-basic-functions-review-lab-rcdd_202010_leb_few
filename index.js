90299d8083bb2dfc902ca31fb9f35fb7080b80a5
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-10 09:30:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const saturdayFun = (activity = "roller-skate") => {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = "go to the office") => {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (flair = "*") => { 
+  return function(arg = "special"){
+    return `You are ${flair}${arg}${flair}!`
+  }
+}
+
+const Calculator = {
+  add: (a,b) => a + b,
+  subtract: (a,b) => a - b,
+  multiply: (a,b) => a * b,
+  divide: (a,b) => a / b
+}
+
+const actionApplyer = (start, arr) => {
+  let a = start
+
+  for (let instance of arr){
+    a = instance(a)
+  }
+  return a
+}
